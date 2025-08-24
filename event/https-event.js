a79ad598fd08d0f8136f6628cd17692ee167e7d4
faisalsaificode <faisalsaifi.code@gmail.com>
2025-08-24 14:23:32 +0530

    commit

---
+++ event/https-event.js
@@ -0,0 +1,22 @@
+const http = require('http')
+
+const server = http.createServer((req, res) =>{
+    if(req.method=='POST'){
+        //expecting data from client\
+        let body = "";
+        req.on('data',(chunk)=>{
+            body += chunk.toString()
+        })
+
+        req.on('end', ()=>{
+            console.log(body);
+            res.end('Data is Received');
+        })
+    }else{
+    res.end('Welcome to node JS')
+    }
+})
+
+server.listen(3100)
+
+console.log('server is listen on 3100')
